604b8b874845138965e7619cb699f4319555ada3
Jobir <toshtemirovjobir6@gmail.com>
2024-09-29 19:21:31 +0500

    added

--- lesson6/book_project/books/models.py
+++ lesson6/book_project/books/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
-
 class Book(models.Model):
     objects = None
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    description = models.TextField()
+    author = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
     published_date = models.DateField()
 
     def __str__(self):
--- lesson6/book_project/books/views.py
+++ lesson6/book_project/books/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-from django.http import HttpResponse
 
 
 def book_list_view(request):
@@ -18,15 +14,18 @@ def book_add_view(request):
         description = request.POST.get('description')
         published_date = request.POST.get('published_date')
 
-        Book.objects.create(title=title, author=author, description=description, published_date=published_date)
+        Book.objects.create(
+            title=title,
+            author=author,
+            description=description,
+            published_date=published_date
+        )
         return redirect('book_list')
-
-    return render(request, 'books/book_add.html')
+    return render(request, 'books/book_form.html')
 
 
 def book_edit_view(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-
     if request.method == 'POST':
         book.title = request.POST.get('title')
         book.author = request.POST.get('author')
@@ -34,15 +33,10 @@ def book_edit_view(request, book_id):
         book.published_date = request.POST.get('published_date')
         book.save()
         return redirect('book_list')
-
-    return render(request, 'books/book_edit.html', {'book': book})
+    return render(request, 'books/book_form.html', {'book': book})
 
 
 def book_delete_view(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-
-    if request.method == 'POST':
-        book.delete()
-        return redirect('book_list')
-
-    return render(request, 'books/book_delete.html', {'book': book})
+    book.delete()
+    return redirect('book_list')
