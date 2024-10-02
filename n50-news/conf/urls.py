2c1576cf41cbfc24840ea93fe8f8e98d01447f80
Jobir <toshtemirovjobir6@gmail.com>
2024-10-02 15:12:46 +0500

    abv

--- n50-news/conf/urls.py
+++ n50-news/conf/urls.py
@@ -3,9 +3,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from forms.views import create_user_info_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('news.urls')),
+    path('form/', include('forms.urls', namespace="form")),
+    path('', include('news.urls', namespace="news")),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
